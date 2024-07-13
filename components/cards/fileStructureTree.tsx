dce52ba9380fb5af895eb419f617d640f9737daf
Ankit Biswas <varanasiartist.omg@gmail.com>
2024-07-13 23:10:50 +0530

    file context menu initiated

--- components/cards/fileStructureTree.tsx
+++ components/cards/fileStructureTree.tsx
@@ -1,56 +1,80 @@
 'use client'
-import {useEffect, useState} from 'react'
-
+import { useEffect, useState } from 'react';
 
 const ws = new WebSocket(process.env.NEXT_PUBLIC_SOCKET_BACKEND_URL || 'ws://localhost:5001');
 
-const FileTreeNode = ({ fileName, nodes, onSelect, path, searchSelectedPath, setSearchResult, searchResult}: any) => {
+const FileTreeNode = ({ fileName, nodes, onSelect, path, searchSelectedPath, setSearchResult, searchResult }: any) => {
     const [isExpanded, setIsExpanded] = useState(false);
+    const [menuStyle, setMenuStyle] = useState<any>({ display: 'none', top: 0, left: 0 });
     const isFolder = nodes !== null;
 
-    useEffect(()=>{
-        if(searchSelectedPath===""){
-            setSearchResult([])
+    useEffect(() => {
+        if (searchSelectedPath === "") {
+            setSearchResult([]);
         }
-        if(searchSelectedPath!==""){
-            setIsExpanded(true)
+        if (searchSelectedPath !== "") {
+            setIsExpanded(true);
         }
-        if(path.includes(searchSelectedPath) && !isFolder){
+        if (path.includes(searchSelectedPath) && !isFolder) {
             if (!searchResult.includes(path)) {
-                setSearchResult([...searchResult, path])
+                setSearchResult([...searchResult, path]);
             }
-            if(searchSelectedPath === ""){
-                setSearchResult([])
+            if (searchSelectedPath === "") {
+                setSearchResult([]);
             }
         }
-        
+
         return () => {
             if (searchSelectedPath === "") {
-              setIsExpanded(false);
+                setIsExpanded(false);
             }
         };
 
-    }, [searchSelectedPath])
+    }, [searchSelectedPath]);
 
     const toggleExpansion = () => {
         setIsExpanded(!isExpanded);
     };
 
-    
+    const handleRightClick = (event: any, filePath: any) => {
+        event.preventDefault();
+        setMenuStyle({
+            display: 'block',
+            top: event.clientY,
+            left: event.clientX
+        });
+    };
 
+    const handleMenuClick = (action: any, filePath: any) => {
+        console.log(action, filePath);
+        setMenuStyle({ display: 'none' });
+        // Perform actions based on the selected option
+    };
+
+    const handleClick = () => {
+        setMenuStyle({ display: 'none' });
+    };
 
     return (
-        <div className='' style={{ marginLeft: '17px'}} >
+        <div className='' style={{ marginLeft: '17px', position: 'relative' }} onClick={handleClick}>
             {isFolder ? (
-                <div style={{borderLeft:"0.1px solid #7e7e7e"}}>
-                    <span className='text-white-2 hover:text-blue font-thin text-small-regular' onClick={toggleExpansion} style={{ cursor: 'pointer'}}>
-                        <span className='pr-2' style={{fontSize:"10px"}}>{isExpanded ? '▼ 📂' : '▶ 📁'}</span>{fileName}
+                <div style={{ borderLeft: "0.1px solid #7e7e7e" }}>
+                    <span className='text-white-2 hover:text-blue font-thin text-small-regular' onClick={toggleExpansion} style={{ cursor: 'pointer' }}>
+                        <span className='pr-2' style={{ fontSize: "10px" }}>{isExpanded ? '▼ 📂' : '▶ 📁'}</span>{fileName}
                     </span>
                     {isExpanded && (
-                        <ul style={{ listStyleType: 'none'}}>
+                        <ul style={{ listStyleType: 'none' }}>
                             {Object.keys(nodes).map((child) => (
-                                <li key={child} style={{lineHeight:"20px"}}>
-                                    <FileTreeNode fileName={child} nodes={nodes[child]} onSelect={onSelect} path={path+'/'+child} searchSelectedPath={searchSelectedPath} setSearchResult={setSearchResult} searchResult={searchResult}/>
+                                <li key={child} style={{ lineHeight: "20px" }}>
+                                    <FileTreeNode
+                                        fileName={child}
+                                        nodes={nodes[child]}
+                                        onSelect={onSelect}
+                                        path={`${path}/${child}`}
+                                        searchSelectedPath={searchSelectedPath}
+                                        setSearchResult={setSearchResult}
+                                        searchResult={searchResult}
+                                    />
                                 </li>
                             ))}
                         </ul>
@@ -58,42 +82,52 @@ const FileTreeNode = ({ fileName, nodes, onSelect, path, searchSelectedPath, set
                 </div>
             ) : (
                 <div className="">
-                    <span className='cursor-pointer font-thin text-small-regular hover:text-blue' onClick={(e)=>{
-                        onSelect(path)
-                    }}> <span className='pr-2 ' style={{fontSize:"10px"}}>📄</span> {fileName}</span>
+                    <span
+                        className='cursor-pointer font-thin text-small-regular hover:text-blue'
+                        onClick={() => onSelect(path)}
+                        onContextMenu={(e) => handleRightClick(e, path)}
+                    >
+                        <span className='pr-2' style={{ fontSize: "10px" }}>📄</span> {fileName}
+                    </span>
                 </div>
             )}
+
+            <div className='text-black-1' style={{ ...menuStyle, position: 'fixed', backgroundColor: 'white', border: '1px solid black', zIndex: 1000 }}>
+                <div className='text-black-1 hover:bg-black-3' style={{ padding: '10px', cursor: 'pointer' }} onClick={() => handleMenuClick('download', path)}>Download</div>
+                <div className='text-black-1 hover:bg-black-3' style={{ padding: '10px', cursor: 'pointer' }} onClick={() => handleMenuClick('rename', path)}>Rename</div>
+                <div className='text-black-1 hover:bg-black-3' style={{ padding: '10px', cursor: 'pointer' }} onClick={() => handleMenuClick('delete', path)}>Delete</div>
+                <div className='text-black-1 hover:bg-black-3' style={{ padding: '10px', cursor: 'pointer' }} onClick={() => handleMenuClick('openInEditor', path)}>Open in Editor</div>
+            </div>
         </div>
     );
 };
 
+const FileStructureTree = ({ onSelect, pId, searchSelectedPath, setSearchResult, searchResult }: any) => {
+    const [tree, setTree] = useState<any>(null);
 
-const FileStructureTree = ({onSelect, pId, searchSelectedPath, setSearchResult, searchResult}:any) => {
-    const[tree, setTree]=useState<any>(null)
-
-    useEffect(()=>{
-        async function fetchFileTree(){
-            try{
-                const response=await fetch('http://localhost:5001/files')
-                const fileTree=await response.json()
-                
-                setTree(fileTree)
-            }catch(err){
-                console.log("error in fetching in the recently changed file", err)
+    useEffect(() => {
+        async function fetchFileTree() {
+            try {
+                const response = await fetch('http://localhost:5001/files');
+                const fileTree = await response.json();
+                setTree(fileTree);
+            } catch (err) {
+                console.log("error in fetching in the recently changed file", err);
             }
         }
-        fetchFileTree()
+        fetchFileTree();
 
-        ws.onmessage=async(event)=>{
-            const message=JSON.parse(event.data)
-            if(message.type==='file:refresh'){
-                fetchFileTree()
+        ws.onmessage = async (event) => {
+            const message = JSON.parse(event.data);
+            if (message.type === 'file:refresh') {
+                fetchFileTree();
             }
         }
-    })
+    }, []);
+
     return (
         <div>
-            <FileTreeNode 
+            <FileTreeNode
                 fileName={'/'}
                 nodes={tree?.tree[pId]}
                 onSelect={onSelect}
@@ -103,7 +137,7 @@ const FileStructureTree = ({onSelect, pId, searchSelectedPath, setSearchResult, 
                 searchResult={searchResult}
             />
         </div>
-    )
+    );
 }
 
-export default FileStructureTree
+export default FileStructureTree;
